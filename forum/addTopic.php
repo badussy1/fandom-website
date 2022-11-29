e4e91b09e2e6b02f319f8b99ed2f642e8e902622
MIA ISABELLA ISABELLA <119285524+badussy1@users.noreply.github.com>
2022-11-29 19:05:10 +0000

    saving for v1

--- forum/addTopic.php
+++ forum/addTopic.php
@@ -1,29 +1,5 @@
 <?php include $_SERVER['DOCUMENT_ROOT'].'/functions.php';
 
 $msg = "<p>The <strong>$topicTitle</strong> topic has been created.</p>";
-?>
-<html>
-  <?php
 echo $msg; 
-  if ($_SESSION["forum"] != NULL)  {
-    echo "this post was written by: ".$_SESSION["forum"]['username']."<br>"; 
-    echo "this post is about: ".$_SESSION["forum"]['topicTitle']."<br>"; 
-    echo "here's what you said about it: ".$_SESSION["forum"]['content']."<br>";  
-  } else {
-    echo "no data exists";
-  }
-
-$newFormData = array(
-        "date"=>date("m/d/Y"),
-        "username"=> $_SESSION["join"]["fName"],
-        "topicTitle"=> $_SESSION["join"]["email"],
-        "content"=> $_SESSION["join"]["age"]
-          );
-$jsonFormData = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/data/forumData.json');
-$pastFormData = json_decode($jsonFormData, TRUE);
-array_push($pastFormData, $newFormData);
-$jsonData = json_encode($pastFormData);
-
-file_put_contents($_SERVER['DOCUMENT_ROOT'].'/data/forumData.json', $jsonData);
-?> 
-</html>
+?>
---
+++ toDo.php
@@ -0,0 +1,7 @@
+1. finish forum 
+2. create topic list
+3. add reply ability 
+4. add images 
+5. contact form 
+6. learn fandom page 
+7. try not to kill yourself 
